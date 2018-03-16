ef040e43de4c408c6bce22fce9ea7f5f242ebb09
Jellz <xdjellz@gmail.com>
2018-03-16 20:47:25 +1100

    lets include the command name in invalid usage message

--- commands/adminpts.js
+++ commands/adminpts.js
@@ -1,11 +1,11 @@
 module.exports = {
     run: async (client, msg, args) => {
         const db = client.db.table('globalPoints');
-        if (!args[0]) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${module.exports.meta.usage}\`\`\``);
+        if (!args[0]) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.prefix + module.exports.meta.name + ' ' + module.exports.meta.usage}\`\`\``);
         if (args[0].toLowerCase() == 'edit') {
             const target = args[1] ? msg.mentions.users.first() || client.users.get(args[1]) : false;
-            if (!target) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${module.exports.meta.usage}\`\`\``);
-            if (!args[2] || isNaN(args[2])) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${module.exports.meta.usage}\`\`\``);
+            if (!target) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.prefix + module.exports.meta.name + ' ' + module.exports.meta.usage}\`\`\``);
+            if (!args[2] || isNaN(args[2])) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.prefix + module.exports.meta.name + ' ' + module.exports.meta.usage}\`\`\``);
             const obj = db.get(target.id).run();
             obj['points'] += args[2];
             db.get(target.id).update(obj).run().then(() => () => {
@@ -13,14 +13,14 @@ module.exports = {
             });
         } else if (args[0].toLowerCase() == 'set') {
             const target = args[1] ? msg.mentions.users.first() || client.users.get(args[1]) : false;
-            if (!target) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${module.exports.meta.usage}\`\`\``);
-            if (!args[2] || isNaN(args[2])) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${module.exports.meta.usage}\`\`\``);
+            if (!target) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.prefix + module.exports.meta.name + ' ' + module.exports.meta.usage}\`\`\``);
+            if (!args[2] || isNaN(args[2])) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.prefix + module.exports.meta.name + ' ' + module.exports.meta.usage}\`\`\``);
             const obj = db.get(target.id).run();
             obj['points'] = args[2];
             db.get(target.id).update(obj).run().then(() => () => {
                 msg.channel.send(`\\✅ Updated points of ${target.tag} (**=${args[2]}**).`);
             });            
-        } else return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${module.exports.meta.usage}\`\`\``);
+        } else return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.prefix + module.exports.meta.name + ' ' + module.exports.meta.usage}\`\`\``);
     },
     meta: {
         name: 'adminpts',
