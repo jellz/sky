6a5676a6a759bd5103f2ed16ce0c2b653ff5e54d
Jellz <xdjellz@gmail.com>
2018-03-21 13:05:17 +1100

    ok i see the issue

--- util/cmdMetaCheck.js
+++ util/cmdMetaCheck.js
@@ -1,6 +1,5 @@
 module.exports = {
-    run: /* async breaks it */ (client, msg, file) => {
-        const meta = file.meta;
+    run: /* async breaks it */ (client, msg, meta) => {
         if (meta.ownerOnly) {
             if (msg.author.id !== client.config.ownerID) return false;
         }
