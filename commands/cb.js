b87a8bb17bc24963bd5086fa1a732ac18560a181
Jellz <xdjellz@gmail.com>
2018-03-21 11:34:14 +1100

    make cleverbot public again

--- commands/cb.js
+++ commands/cb.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     meta: {
         name: 'cb',
-        ownerOnly: true,
+        ownerOnly: false,
         description: 'Talk with the Sky cleverbot.',
         usage: '<%input%>'
     }
