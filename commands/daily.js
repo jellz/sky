7919084ef7a47bd1bfa13b3128ab6e71860c4f39
Jellz <xdjellz@gmail.com>
2018-03-22 15:43:28 +1100

    change daily cmd to 24hrs

--- commands/daily.js
+++ commands/daily.js
@@ -5,7 +5,7 @@ module.exports = {
         const points = await r.table('globalPoints').get(msg.author.id).run();
         if (!points) return msg.channel.send(`\\❌ **${msg.author.tag}** does not have a profile. Start chatting to automatically generate one. ${msg.guild.id == '110373943822540800' ? '(Discord Bots is an ignored guild!)' : ''}`);
         if (points.lastDaily) {
-            const waiting = 1000*60; // 1 minute for testing, switch to 8.64e+7 (24 hours) later.
+            const waiting = 8.64e+7
             const until = new Date().getTime() - points.lastDaily;
             const formatted = moment(new Date().getTime() + (waiting - until)).fromNow();
             if (until < waiting) return msg.channel.send(`\\❌ You've already ran this command today! Try again ${formatted}.`);
