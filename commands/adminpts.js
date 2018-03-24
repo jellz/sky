256b9a993c92291d1b956845354d114b6cde151e
Jellz <xdjellz@gmail.com>
2018-03-24 21:45:03 +1100

    revert

--- commands/adminpts.js
+++ commands/adminpts.js
@@ -7,8 +7,7 @@ module.exports = {
             if (!target) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.config.prefix + module.exports.meta.aliases[0] + ' ' + module.exports.meta.usage}\`\`\``);
             if (!args[2] || isNaN(args[2])) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.config.prefix + module.exports.meta.aliases[0] + ' ' + module.exports.meta.usage}\`\`\``);
             const obj = db.get(target.id).run();
-            const npv = parseInt(args[2]);
-            obj['points'] += npv;
+            obj['points'] += parseInt(args[2]);
             await db.get(target.id).update(obj).run();
             msg.channel.send(`\\✅ Updated points of ${target.tag} (**+${args[2]}**).`);
 
@@ -24,7 +23,7 @@ module.exports = {
             msg.channel.send(`\\✅ Updated points of ${target.tag} (**=${args[2]}**).`);
 
 
-
+            
         } else if (args[0].toLowerCase() == 'take') {
             const target = args[1] ? msg.mentions.users.first() || client.users.get(args[1]) : false;
             if (!target) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.config.prefix + module.exports.meta.aliases[0] + ' ' + module.exports.meta.usage}\`\`\``);
