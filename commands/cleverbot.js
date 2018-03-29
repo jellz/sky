347da174cd5d2239e7801e686ad13bec5fd47868
Jellz <xdjellz@gmail.com>
2018-03-29 20:38:49 +1100

    oh I forgot to make it stop typing

--- commands/cleverbot.js
+++ commands/cleverbot.js
@@ -2,10 +2,11 @@ module.exports = {
     run: async (client, msg, args) => {
         if (!args[0]) return msg.channel.send(`\\❌ Invalid usage. Expected usage:\n\n\`\`\`${client.config.prefix + module.exports.meta.aliases[0] + ' ' + module.exports.meta.usage}\`\`\``);
         const query = args.join(' ');
-        await msg.channel.startTyping();
+        msg.channel.startTyping();
         const cleverbot = require('../index.js').cleverbot;
         await cleverbot.write(query, async function(response) {
             setTimeout(() => {
+                msg.channel.stopTyping();
                 msg.channel.send(`**${msg.author.username}**, ${response.output}`);
             }, 1500)
         });
