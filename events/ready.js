3dedc862797e473417ee01e82eccbb6194416c63
Jellz <xdjellz@gmail.com>
2018-04-03 07:31:21 +1000

    Fixes #16

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = {
     run: async (client) => {
         console.log(`Sky: logged in as ${client.user.tag}!`);
-        client.user.setActivity(';help | https://jellz.fun/sky');
+        client.user.setActivity(client.config.prefix + 'help | https://jellz.fun/sky');
         (await client.db.table('globalPoints').run()).forEach(element => {
             client.tempProfiles[element.id] = { earningPoints: true };
         });
