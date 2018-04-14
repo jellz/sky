5c9037e43b174255f9537b5ff65f6a4690763f1b
Jellz <xdjellz@gmail.com>
2018-04-14 18:55:42 +1000

    import messageattachement

--- commands/wanted.js
+++ commands/wanted.js
@@ -1,5 +1,6 @@
 module.exports = {
     run: async (client, msg, args) => {
+        const { MessageAttachment } = require('discord.js');
         const target = args[0] ? msg.mentions.users.first() || client.users.get(args[0]) : msg.author;
         if (!target) return msg.channel.send('\\❌ Invalid user.');
         await msg.channel.send(new MessageAttachment(await client.idiot.wanted(target.displayAvatarURL({ format: 'png', size: 128 })), 'wanted.png'));
